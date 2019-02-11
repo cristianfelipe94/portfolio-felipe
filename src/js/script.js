5aed988535a9e25bd7f969b395b005283cddcc85
Cristian Felipe <cristianpipe0494@gmail.com>
2019-02-11 14:28:33 -0600

    Have changed classes names and organize code.

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,22 @@
 const helloBtn = document.getElementById('js-hello');
-const landingPage = document.getElementById('js-landingpage');
-const VerticalTabs = document.getElementById('js-tabs-info-wrapper');
+const landingPage = document.getElementById('js-landing-page');
+const VerticalTabs = document.getElementById('js-tabs-wrapper');
 const btnClose = document.getElementById('js-btn-close');
-const contentBlock = document.getElementById('content-block');
 
-const portFolioContent = document.getElementById('js-wrapper-portfolio');
+const contentBlock = document.getElementById('js-content-wrapper');
+const portFolioContent = document.getElementById('js-wrapper-seccion');
 
 helloBtn.addEventListener('click', function () {
-    landingPage.setAttribute('class', 'landingPageMoves');
-    VerticalTabs.setAttribute('class', 'tabs-info-wrapper-showing');
-    contentBlock.setAttribute('class', 'content-page-showing');
-    portFolioContent.setAttribute('class', 'content-inside-wrapper-showing');
+    landingPage.setAttribute('class', 'landing-wrapper-hidden');
+    VerticalTabs.setAttribute('class', 'tabs-wrapper-showing');
+    contentBlock.setAttribute('class', 'content-wrapper-showing');
+    portFolioContent.setAttribute('class', 'content-page-showing');
 });
 
 btnClose.addEventListener('click', function () {
     console.log('closing');
-    landingPage.setAttribute('class', 'landing-page-wrapper');
-    VerticalTabs.setAttribute('class', 'tabs-info-wrapper-hidden');
-    contentBlock.setAttribute('class', 'content-page-hidden');
-    portFolioContent.setAttribute('class', 'content-inside-wrapper-hidden');
+    landingPage.setAttribute('class', 'landing-wrapper-showing');
+    VerticalTabs.setAttribute('class', 'tabs-wrapper-hidden');
+    contentBlock.setAttribute('class', 'content-wrapper-hidden');
+    portFolioContent.setAttribute('class', 'content-page-hidden');
 });
