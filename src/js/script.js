831e9d0de8c2d04752f645f58051a903359e901d
Cristian Felipe <cristianpipe0494@gmail.com>
2019-02-13 19:31:52 -0600

    Have added icons on portfolio to distinguish projects

--- src/js/script.js
+++ src/js/script.js
@@ -29,26 +29,32 @@
         imgPort1 = {
             src: '../img/canvas_game_port1.jpg',
             alt: 'canvas mini game picture',
+            type: 'game',
         },
         imgPort2 = {
             src: '../img/derrota_palabra_port1.jpg',
             alt: 'derrota la palabra game picture',
+            type: 'game',
         },
         imgPort3 = {
             src: '../img/tiny_one_port1.jpg',
             alt: 'tiny one picture',
+            type: 'page',
         },
         imgPort4 = {
             src: '../img/folio_port1.jpg',
             alt: 'folio picture',
+            type: 'page',
         },
         imgPort5 = {
             src: '../img/blaz_port1.jpg',
             alt: 'blaz game picture',
+            type: 'page',
         },
         imgPort6 = {
             src: '../img/cuda_port1.jpg',
             alt: 'cuda game picture',
+            type: 'page',
         },
     ];
 
@@ -117,7 +123,19 @@
     imgPortArray.forEach(element => {
         const elementSrc = element.src;
         const elementAlt = element.alt;
+        const elementTypeOf = element.type;
         const elementPort = document.createElement('img');
+        const portElementWrapper = document.createElement('div');
+        portElementWrapper.setAttribute('class', 'portfolio-wrappers');
+        if (elementTypeOf === 'game') {
+            const gameLink = document.createElement('a');
+            gameLink.setAttribute('class', 'portfolio-icons fas fa-gamepad');
+            portElementWrapper.appendChild(gameLink);
+        } else if (elementTypeOf === 'page') {
+            const pageLink = document.createElement('a');
+            pageLink.setAttribute('class', 'portfolio-icons fas fa-gamepad');
+            portElementWrapper.appendChild(pageLink);
+        }
         elementPort.setAttribute('src', elementSrc);
         elementPort.setAttribute('alt', elementAlt);
         elementPort.setAttribute('class', 'gallery-port');
@@ -135,7 +153,8 @@
 
             backgroundModal.appendChild(modalElement);
         });
-        portContentBlock.appendChild(elementPort);
+        portElementWrapper.appendChild(elementPort);
+        portContentBlock.appendChild(portElementWrapper);
     });
 
     const maxResponse = 10;
@@ -162,8 +181,6 @@
             activityUrl.setAttribute('href', responseFinalUrl);
             activityUrl.setAttribute('target', '_blank');
             activityUrl.setAttribute('class', 'activity-url-color');
-
-
 
             activityDate.innerText = responseDateFormated;
             activityCommit.innerText = responseMessage;
