3bf05227675601b95260db6f5e4358cd2557ea13
Cristian Felipe <cristianpipe0494@gmail.com>
2019-02-14 09:13:17 -0600

    Have added functionality to portfolio's links, now able to visit portfolio project.

--- src/js/script.js
+++ src/js/script.js
@@ -29,31 +29,37 @@
         imgPort1 = {
             src: '../img/canvas_game_port1.jpg',
             alt: 'canvas mini game picture',
+            href: 'https://cristianfelipe94.github.io/CanvasGame/',
             type: 'game',
         },
         imgPort2 = {
             src: '../img/derrota_palabra_port1.jpg',
             alt: 'derrota la palabra game picture',
+            href: 'https://cristianfelipe94.github.io/Derrota-la-Palabra/',
             type: 'game',
         },
         imgPort3 = {
             src: '../img/tiny_one_port1.jpg',
             alt: 'tiny one picture',
+            href: 'https://cristianfelipe94.github.io/TinyOneWebSite/',
             type: 'page',
         },
         imgPort4 = {
             src: '../img/folio_port1.jpg',
             alt: 'folio picture',
+            href: 'https://cristianfelipe94.github.io/Final-Test./',
             type: 'page',
         },
         imgPort5 = {
             src: '../img/blaz_port1.jpg',
             alt: 'blaz game picture',
+            href: 'https://cristianfelipe94.github.io/BlazRobarWebsite/',
             type: 'page',
         },
         imgPort6 = {
             src: '../img/cuda_port1.jpg',
             alt: 'cuda game picture',
+            href: 'https://cristianfelipe94.github.io/ExamenWeb/',
             type: 'page',
         },
     ];
@@ -123,6 +129,7 @@
     imgPortArray.forEach(element => {
         const elementSrc = element.src;
         const elementAlt = element.alt;
+        const elementUrl = element.href;
         const elementTypeOf = element.type;
         const elementPort = document.createElement('img');
         const portElementWrapper = document.createElement('div');
@@ -130,10 +137,14 @@
         if (elementTypeOf === 'game') {
             const gameLink = document.createElement('a');
             gameLink.setAttribute('class', 'portfolio-icons fas fa-gamepad');
+            gameLink.setAttribute('href', elementUrl);
+            gameLink.setAttribute('target', '_blank');
             portElementWrapper.appendChild(gameLink);
         } else if (elementTypeOf === 'page') {
             const pageLink = document.createElement('a');
-            pageLink.setAttribute('class', 'portfolio-icons fas fa-gamepad');
+            pageLink.setAttribute('class', 'portfolio-icons fas fa-eye');
+            pageLink.setAttribute('href', elementUrl);
+            pageLink.setAttribute('target', '_blank');
             portElementWrapper.appendChild(pageLink);
         }
         elementPort.setAttribute('src', elementSrc);
